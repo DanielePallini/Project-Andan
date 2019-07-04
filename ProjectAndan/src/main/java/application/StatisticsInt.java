c600e90ffdb810c4ec53a2c65de88b025d7afdd0
DanielePallini <48405464+DanielePallini@users.noreply.github.com>
2019-07-04 15:17:15 +0200

    Correzione ridondanze

---
+++ ProjectAndan/src/main/java/application/StatisticsInt.java
@@ -0,0 +1,88 @@
+package application;
+
+
+import java.util.Vector;
+import java.util.Collections;
+
+
+public class StatisticsInt {
+	private static Vector<Integer> v = new Vector<Integer>();
+	private static Vector<Statistiche> statistiche = new Vector<Statistiche>();
+
+		
+public static Vector<Integer> getVett(String field){
+	Vector<Tumore> g = Serialization.data();
+	switch (field) {
+	
+	case  "maschi" : {Vector<Integer> v = new Vector<Integer>();
+	for (Tumore item : g ) {
+		v.add(item.getMaschi()); 
+		}
+	return v;
+	} 
+	case "femmine" : {Vector<Integer> v = new Vector<Integer>();
+	for (Tumore item : g ) {
+		v.add(item.getFemmine()); 
+		}
+	return v;
+	} 
+   }
+	return v;
+}
+		
+public static int somma (String field) {
+	Vector<Integer> vett = getVett(field);
+    int sum=0;
+    for (Integer item : vett) {
+	 sum += item ;
+}
+return sum;
+}
+	
+public static float avg (String field) {
+	Vector<Integer> vett = getVett(field);
+	int dim= vett.size();
+	int sum= somma(field);
+	float average = (float) sum / dim;
+	return average;
+}
+	
+public static Object max (String field) {
+	Vector<Integer> vett = getVett(field);
+	Object obj = Collections.max(vett);
+	return obj;
+	}
+
+public static Object min (String field) {
+	Vector<Integer> vett = getVett(field);
+	Object obj = Collections.min(vett);
+    return obj;
+    }
+
+public static double getVariance(String field) {
+	Vector<Integer> vett = getVett(field);
+	int dim = vett.size();
+	float avg = avg(field);
+    double temp = 0;
+    for(float item :vett)
+        temp += (item-avg)*(item-avg);
+    return temp/(dim-1);
+}
+
+public static double getStdDev(String field) {
+    return Math.sqrt(getVariance(field));
+}
+
+public static long getCount(String field) {
+    Vector<Integer> vett = getVett(field);
+    long count = vett.stream().distinct().count();
+    return count;
+}
+
+public static Vector<Statistiche> getStats(String field) {
+	Statistiche v1 = new Statistiche(field , avg(field), min(field), max(field), getStdDev(field),somma(field), getCount(field))	; 	
+	statistiche.add(v1);
+	return statistiche;
+}
+
+}
