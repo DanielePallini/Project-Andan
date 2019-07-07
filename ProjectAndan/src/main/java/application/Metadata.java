8ea9f9514d8ee42b1c3b3cd605dc875c99300aa9
andreagiuliani1 <50946406+andreagiuliani1@users.noreply.github.com>
2019-07-07 14:37:41 +0200

    Add files via upload

--- ProjectAndan/src/main/java/application/Metadata.java
+++ ProjectAndan/src/main/java/application/Metadata.java
@@ -1,5 +1,11 @@
-package application;
+package data;
 
+/**
+ * Classe che descrive i metadati
+ * 
+ * @author Andrea Giuliani Daniele Pallini
+ *
+ */
 
 public class Metadata {
     private String alias;
@@ -8,6 +14,14 @@ public class Metadata {
 	public Metadata() {
 		
 	}
+	/**
+	 * 
+	 * Costruttore di un oggetto metadata
+	 * 
+	 * @param alias
+	 * @param sourceField
+	 * @param type
+	 */
 	public Metadata(String alias, String sourceField, String type) {
 	
 		this.alias = alias;
@@ -15,22 +29,51 @@ public class Metadata {
 		this.type = type;
 	}
 	
+	/**
+	 * Restituisce l'alias del campo considerato
+	 * @return alias l'alias del campo
+	 */
 
 	public String getAlias() {
 		return alias;
 	}
+	
+	/**
+	 * Imposta l'alias 
+	 * @param alias
+	 */
 	public void setAlias(String alias) {
 		this.alias = alias;
 	}
+	
+	/**
+	 * Restituisce il nome del campo considerato nel file
+	 * @return sourceField nome del campo
+	 */
 	public String getSourceField() {
 		return sourceField;
 	}
+	
+	/**
+	 * Imposta il nome del campo nell'oggetto metadata
+	 * @param sourceField
+	 */
 	public void setSourceField(String sourceField) {
 		this.sourceField = sourceField;
 	}
+	
+	/**
+	 * Restituisce il tipo dei dati del campo considerato
+	 * @return type tipo dei dati del campo
+	 */
 	public String getType() {
 		return type;
 	}
+	
+	/**
+	 * Imposta il tipo dei dati del campo considerato
+	 * @param type
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
